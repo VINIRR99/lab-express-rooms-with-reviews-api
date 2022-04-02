d128595f70bb127743127a50a93a3f69464c4c2e
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-04-02 14:35:26 -0400

    feat(signup/routes): handle error messages

--- routes/auth-routes/index.js
+++ routes/auth-routes/index.js
@@ -13,13 +13,25 @@ router.post("/signup", async (req, res) => {
     try {
         await checkSignupInputs(req.body, unfilledInput, differentPasswords, usedUsername);
         const { name, username, password } = await req.body;
+
         const fixedName = await fixName(name);
         const passwordHash = await generatePasswordHash(password);
+
         const payload = await createUser(fixedName, username, passwordHash);
         const token = sign(payload, process.env.SECRET_JWT, { expiresIn: "1day" });
         res.status(200).json({ payload, token });
     } catch (error) {
-        res.status(500).json({ error: error.message })
+        switch (error.message) {
+            case unfilledInput:
+            case differentPasswords:
+                res.status(400).json({ error: error.message });
+                break;
+            case usedUsername:
+                res.status(409).json({ error: error.message });
+                break;
+            default:
+                res.status(500).json({ error: error.message });
+        };
     };
 });
 
