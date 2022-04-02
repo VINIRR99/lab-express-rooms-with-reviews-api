90675e15c0132bbcffc1a3337bb2a2f4bb6265ef
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-04-02 16:03:18 -0400

    feat(login/routes): create checkLogin function

--- routes/auth-routes/functions.js
+++ routes/auth-routes/functions.js
@@ -1,6 +1,6 @@
 const User = require("../../models/User.models");
 
-const { genSalt, hash } = require("bcryptjs");
+const { genSalt, hash, compare } = require("bcryptjs");
 
 module.exports = {
     checkSignupInputs: async (bodyRequest, unfilledInputMsg, differentPasswordsMsg, usedUsernameMsg) => {
@@ -28,5 +28,24 @@ module.exports = {
             password: passwordHash
         });
         return { _id, name, username, rooms, reviews };
+    },
+    checkLogin: async (usernameInput, password, invalidLoginMsg) => {
+        const user = await User.findOne({ username: usernameInput },
+            {
+                name: 1,
+                username: 1,
+                password: 1,
+                rooms: 1,
+                reviews: 1
+            }
+        );
+        if (!user) throw new Error(invalidLoginMsg);
+
+        const { name, username, password: passwordHash, rooms, reviews } = await user;
+
+        const compareHash = await compare(password, passwordHash);
+        if (!compareHash) throw new Error(invalidLoginMsg);
+
+        return {name, username, rooms, reviews};
     }
 };
--- routes/auth-routes/index.js
+++ routes/auth-routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-const { checkSignupInputs, generatePasswordHash, fixName, createUser } = require("./functions");
+const { checkSignupInputs, generatePasswordHash, fixName, createUser, checkLogin } = require("./functions");
 
 const { sign } = require("jsonwebtoken");
 
@@ -35,25 +35,13 @@ router.post("/signup", async (req, res) => {
     };
 });
 
-const User = require("../../models/User.models");
-
-const { compare } = require("bcryptjs");
-
 router.post("/login", async (req, res) => {
     const invalidLogin = "Username or password is invalid!";
 
     try {
-        const { username: usernameInput, password } = await req.body;
+        const { username, password } = await req.body;
 
-        const user = await User.findOne({ username: usernameInput }, { name: 1, password: 1, rooms: 1, reviews: 1 });
-        if (!user) throw new Error(invalidLogin);
-
-        const { _id, name, username, password: passwordHash, rooms, reviews } = await user;
-
-        const compareHash = await compare(password, passwordHash);
-        if (!user || !compareHash) throw new Error(invalidLogin);
-
-        const payload = { _id, name, username, rooms, reviews };
+        const payload = await checkLogin(username, password, invalidLogin);
         const token = sign(payload, process.env.SECRET_JWT, { expiresIn: "1day" });
 
         res.status(200).json({ payload, token });
